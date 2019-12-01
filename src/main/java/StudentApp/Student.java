6ad598fdeda864c45311a7ad9f57dba8c51ff699
Yaroslava Bondarchuk <yaroslava7bondarchuk@gmail.com>
2019-12-02 01:20:57 +0200

    Review fixes

--- src/main/java/StudentApp/Student.java
+++ src/main/java/StudentApp/Student.java
@@ -3,47 +3,47 @@ package StudentApp;
 import java.util.ArrayList;
 
 public class Student {
+    private int id;
     private String name;
     private String familyName;
-    private int id;
-    private static int numStudent;
-    ArrayList<Integer> marks;
-    double averageMark;
+    private ArrayList<Integer> marks;
+    private double averageMark;
 
-    public Student (String name, String familyName){
+    public Student(int id, String name, String familyName) {
+        this.id = id;
         this.name = name;
         this.familyName = familyName;
-        id = numStudent;
-        numStudent++;
     }
 
     public String toString() {
-        return "Student's info: \n name- "+ this.name + "\n family name - " + this.familyName+ "\n id -" + this.id;
+        return "Student's info: \n name- " + this.name + "\n family name - " + this.familyName + "\n id -" + this.id;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public String getFamilyName() {
-        return familyName;
+        return this.familyName;
     }
 
-    public double getAverageMark(ArrayList<Integer> marks) {
-        return averageMark;
+    public double getAverageMark() {
+        return this.averageMark;
     }
 
-    public void setMarks (int mark){
-       this.marks.add(mark);
+    public void setMarks(int mark) {
         int sum = 0;
-        for (Integer el : marks) {
-            sum += el;
+
+        this.marks.add(mark);
+
+        for (Integer grade : marks) {
+            sum += grade;
         }
-        averageMark = (double) sum / marks.size();
+
+        this.averageMark = (double) sum / this.marks.size();
     }
 
-    public String getInfo (){
+    public String getInfo() {
         return this.toString();
     }
-
 }
